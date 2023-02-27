78898c52cda9f61641a26475ccd689f3ee762327
Neelapallipallavi <123191428+Neelapallipallavi@users.noreply.github.com>
2023-02-27 14:01:39 +0530

    Add files via upload

---
+++ project_2/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+
+
+st.title('Pallavi Neelapalli')
+st.header('*:red[Data Science Intern:sunglasses:]*')
+st.subheader(":blue[Innomatics Research labs]")
+
+btn_click = st.button("To know more details about me...click here")
+if btn_click == True:
+    st.subheader("wow! I'm glad that.. you wanna know about me ")
+    st.caption('*I am a recent graduate and intrested in datascience*')
+    
+    st.subheader('Find my linkedin and Github profiles here :point_down:')
+   
+
+    st.markdown('**Linkedin: www.linkedin.com/in/neellapalli-pallavi-ba17a7240/**')
+    st.markdown('**GitHub: https://github.com/Neelapallipallavi**')
